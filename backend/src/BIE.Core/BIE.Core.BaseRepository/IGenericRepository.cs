5512d5a5b4ca97f97c0c877551f47bd4bdebde5a
Ahsan.Khan <Ahsan.Khan@LPTP-308>
2024-05-02 00:00:01 +0500

    Initialized BE API Project
    Signed-off-by: Muhammad Ahsan Khan <ahsan.m.khan@fau.de>

---
+++ backend/src/BIE.Core/BIE.Core.BaseRepository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BIE.Core.BaseRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(long id);
+
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
