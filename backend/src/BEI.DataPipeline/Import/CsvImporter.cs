0a0f61f45436015cfa3f95a7960fe6f65f546cf6
Leon Holtmeier <l.holtmeier@campus.tu-berlin.de>
2024-05-05 23:00:38 +0200

    Datapipeline Initial Template
    Signed-off-by: Leon Holtmeier <l.holtmeier@campus.tu-berlin.de>

---
+++ backend/src/BEI.DataPipeline/Import/CsvImporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BEI.DataPipeline.Data;
+
+namespace BEI.DataPipeline.Import
+{
+    internal class CsvImporter : IImporter
+    {
+        private string mFileName;
+        CsvImporter(string filepath)
+        {
+
+        }
+
+        public async Task<ITableData> GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/src/BEI.DataPipeline/Import/IImporter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BEI.DataPipeline.Data;
+
+namespace BEI.DataPipeline.Import
+{
+    internal interface IImporter
+    {
+        // this we can implement as async
+        public Task<ITableData> GetData();
+    }
+}
