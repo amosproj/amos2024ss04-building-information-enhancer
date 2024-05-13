e301ff0bbd8e7e6fd105438d0eeb425d509fdefd
Nicolas Bandel <nicolas.bandel@fau.de>
2024-05-13 09:11:18 +0200

    Issue #57: Open CSV-file print content to console
    Signed-off-by: Nicolas Bandel <nicolas.bandel@fau.de>

--- backend/src/BIE.DataPipeline/Import/CsvImporter.cs
+++ backend/src/BIE.DataPipeline/Import/CsvImporter.cs
@@ -1,18 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using BEI.DataPipeline.Data;
+using Microsoft.VisualBasic.FileIO;
 
 namespace BEI.DataPipeline.Import
 {
     internal class CsvImporter : IImporter
     {
         private string mFileName;
-        CsvImporter(string filepath)
+        private TextFieldParser parser;
+        public CsvImporter(string filepath)
         {
+            //YAML Arguments:
+            string delimiter = ";";
+            //TODO checkPath
 
+            //Setup Parser
+            parser = new TextFieldParser(filepath);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(delimiter);
+
+            //Test Read File
+            int noLines = 10;
+            int line = 0;
+            while (!parser.EndOfData && line < noLines)
+            {
+                //Processing row
+                string[] fields = parser.ReadFields();
+                foreach (string field in fields)
+                {
+                    //TODO: Process field
+                    Console.WriteLine(string.Format("Filed {0}", field));
+                }
+                line++;
+            }
         }
 
         public async Task<ITableData> GetData()
--- backend/src/BIE.DataPipeline/Program.cs
+++ backend/src/BIE.DataPipeline/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BEI.DataPipeline.Import;
+
 Console.WriteLine("Hello, World!");
+
+CsvImporter csvImporter = new CsvImporter(@"C:\Users\nicol\Desktop\uni\M3\AMOS\Datasources\Ladesaeulenregister.csv");
