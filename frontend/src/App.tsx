a4dadda7e6a9a309f5cc45880ab7caa91eef0edd
Lucas Nandico <lucas.nandico@fau.de>
2024-05-13 11:54:59 +0200

    initial test dialog with children as prop
    Signed-off-by: Lucas Nandico <lucas.nandico@fau.de>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css'
+import "./App.css";
+import PopupDialog from "./PopupDialog";
 
 function App() {
-  return <h1> Building Information Enhancer</h1>;
+  return (
+    <div>
+      <h1> Building Information Enhancer</h1>
+      <PopupDialog>
+        <p>Hello world</p>
+      </PopupDialog>
+    </div>
+  );
 }
 
-export default App
+export default App;
