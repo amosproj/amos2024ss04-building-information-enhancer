348380fb843325ca621d539b5425aa141430176d
Emil Balitzki <emil.balitzki@gmail.com>
2024-05-13 18:17:04 +0200

    Testing build process
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return <h1> Building Information Enhancer</h1>;
+  return <h1> Building Information Enhancer v.10</h1>;
 }
 
-export default App
+export default App;
