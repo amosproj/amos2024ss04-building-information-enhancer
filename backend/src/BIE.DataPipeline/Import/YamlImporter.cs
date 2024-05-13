0774d00466bba2d7059f37a6797add24dbc4b051
Leon Holtmeier <l.holtmeier@campus.tu-berlin.de>
2024-05-13 17:10:28 +0200

    adding yaml Parser and Object description
    Signed-off-by: Leon Holtmeier <l.holtmeier@campus.tu-berlin.de>

---
+++ backend/src/BIE.DataPipeline/Import/YamlImporter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace BIE.DataPipeline.Import
+{
+    internal class YamlImporter
+    {
+        public static DataSourceDescription GetSourceDescription(string path)
+        {
+            path = Path.GetFullPath(path);
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("file: " + path + " could not be found.");
+            }
+
+            if (!path.EndsWith(".yaml"))
+            {
+                throw new FormatException(path + "is not a yaml file");
+            }
+
+            var yaml = File.ReadAllText(path);
+
+            var deserializer = new Deserializer();
+
+            var description = deserializer.Deserialize<DataSourceDescription>(yaml);
+
+            return description;
+        }
+    }
+
+    internal struct DataSourceDescription
+    {
+        public DataSourceLocation source;
+        public DataSourceOptions options;
+        public string table_name;
+        public List<DataSourceColumn> table_cols;
+
+        internal struct DataSourceLocation
+        {
+            public string type;
+            public string location;
+        }
+
+        internal struct DataSourceOptions
+        {
+            public int skip_lines;
+            public bool discard_null_rows;
+        }
+
+        internal struct DataSourceColumn
+        {
+            public string name;
+            public string name_in_table;
+            public string type;
+        }
+
+    }
+}
--- backend/src/BIE.DataPipeline/Program.cs
+++ backend/src/BIE.DataPipeline/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using BIE.DataPipeline.Import;
+
 Console.WriteLine("Hello, World!");
+
+var description = YamlImporter.GetSourceDescription(args[0]);
+
+Console.WriteLine(description.source.location);
+Console.WriteLine(description.table_name);
+Console.WriteLine(description.table_cols[0].name_in_table);
+
+Console.ReadKey();
