d58a4f429a9657e938f90a802f830bc079bb0e04
Leon Holtmeier <l.holtmeier@campus.tu-berlin.de>
2024-05-13 19:54:08 +0200

    adding nullable attribute
    Signed-off-by: Leon Holtmeier <l.holtmeier@campus.tu-berlin.de>

--- backend/src/BIE.DataPipeline/Program.cs
+++ backend/src/BIE.DataPipeline/Program.cs
@@ -8,6 +8,6 @@ var description = YamlImporter.GetSourceDescription(args[0]);
 
 Console.WriteLine(description.source.location);
 Console.WriteLine(description.table_name);
-Console.WriteLine(description.table_cols[0].type);
+Console.WriteLine(description.table_cols[1].type);
 
 Console.ReadKey();
