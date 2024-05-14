0f28a2a10a989075367de72a692a262fe9f91026
Muhammad Ahsan Khan <ahsan.m.khan@fau.de>
2024-05-14 14:00:17 +0500

    Refactoring. DataPipeline will now insert entire CSV file.
    Signed-off-by: Muhammad Ahsan Khan <ahsan.m.khan@fau.de>
    Co-authored-by: Nicolas Bandel <nicolas.bandel@fau.de>

--- backend/src/BIE.DataPipeline/Program.cs
+++ backend/src/BIE.DataPipeline/Program.cs
@@ -4,31 +4,23 @@ using BIE.DataPipeline;
 using BIE.DataPipeline.Import;
 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Parser Started");
 DataSourceDescription description = YamlImporter.GetSourceDescription(args[0]);
 
 
 DBHelper.CreateDBConnection();
 DBHelper.CreateTable(description);
 
-//Console.WriteLine(description.source.location);
-//Console.WriteLine(description.table_name);
-//Console.WriteLine(description.table_cols[0].type);
-
-//Console.ReadKey();
-
-
 CsvImporter csvImporter = new CsvImporter(description);
-//CsvImporter csvImporter = new CsvImporter(@"C:\Users\nicol\Desktop\uni\M3\AMOS\Datasources\Ladesaeulenregister.csv");
-//CsvImporter csvImporter = new CsvImporter(@"https://data.bundesnetzagentur.de/Bundesnetzagentur/SharedDocs/Downloads/DE/Sachgebiete/Energie/Unternehmen_Institutionen/E_Mobilitaet/Ladesaeulenregister.csv");
+
 Console.WriteLine(csvImporter.GetHeaderString());
 string line = "";
 bool notEOF = csvImporter.ReadLine(out line);
-int row = 0;
-while  (notEOF && row < 10)
+
+while  (notEOF)
 {
     DBHelper.InsertData(csvImporter.GetTableName(),csvImporter.GetHeaderString(),line);
-    Console.WriteLine(line);
-    row++;
     notEOF = csvImporter.ReadLine(out line);
 }
+
+Console.WriteLine("Parser End");
