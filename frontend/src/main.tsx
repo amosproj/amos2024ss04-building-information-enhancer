9a34f158b82bfc060e03a3bedcb7d8d13a5d6acc
Emil Balitzki <emil.balitzki@gmail.com>
2024-05-21 12:00:06 +0200

    Started working on the Map Context
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { TabsContextProvider } from "./contexts/TabsContext.tsx";
+import { MapContextProvider } from "./contexts/MapContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TabsContextProvider>
-      <App />
+      <MapContextProvider>
+        <App />
+      </MapContextProvider>
     </TabsContextProvider>
   </React.StrictMode>
 );
