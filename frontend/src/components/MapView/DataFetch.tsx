a9051518197628a5c75adb7b3b679bea43d361d6
Emil Balitzki <emil.balitzki@gmail.com>
2024-05-23 19:22:22 +0200

    Started working on fixing the FE->BE connection
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/src/components/MapView/DataFetch.tsx
+++ frontend/src/components/MapView/DataFetch.tsx
@@ -4,6 +4,12 @@ import { LatLngBounds } from "leaflet";
 import { useContext, useEffect, useState } from "react";
 import { AlertContext } from "../../contexts/AlertContext";
 const geojsonData: FeatureCollection = data as FeatureCollection;
+import axios from "axios";
+
+// Define the API URL
+const apiUrl = `http://${import.meta.env.VITE_BACKEND_HOST}:${
+  import.meta.env.VITE_BACKEND_PORT
+}/api/v1.0/Dataset/1/data`;
 
 const useGeoData = (
   bounds: LatLngBounds,
@@ -13,27 +19,39 @@ const useGeoData = (
   const { currentAlertCache, setCurrentAlertCache } = useContext(AlertContext);
 
   useEffect(() => {
-    const fetchData = async () => {
+    // Function to send the request
+    const fetchData = async (): Promise<void> => {
       try {
-        // const bottomLat = bounds.getSouth();
-        // const bottomLong = bounds.getWest();
-        // const topLat = bounds.getNorth();
-        // const topLong = bounds.getEast();
-        // TO-DO: Fix so it is not hardcoded
-        const url = `http://localhost:8081/api/v1.0/Dataset/1/data?BottomLat=9&BottomLong=10&TopLat=48&TopLong=49`;
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const result = await response.json();
-        setData(result as FeatureCollection<Geometry>);
+        const bottomLat = bounds.getSouth();
+        const bottomLong = bounds.getWest();
+        const topLat = bounds.getNorth();
+        const topLong = bounds.getEast();
+        console.log(apiUrl);
+        // Define the query parameters
+        const params = {
+          BottomLat: bottomLat,
+          BottomLong: bottomLong,
+          TopLat: topLat,
+          TopLong: topLong,
+          ZoomLevel: zoom,
+        };
+        const response = await axios.get<FeatureCollection<Geometry>>(apiUrl, {
+          params,
+        });
+        setData(response.data as FeatureCollection<Geometry>);
       } catch (error) {
+        // Display alert
         setCurrentAlertCache({
           ...currentAlertCache,
           isAlertOpened: true,
           text: "Fetching data failed, using local GeoJSON data.",
         });
-        console.error("Fetching data failed, using local GeoJSON data.", error);
+        // Console log the error
+        if (axios.isAxiosError(error)) {
+          console.error("Axios error fetching data:", error.message);
+        } else {
+          console.error("Unknown error fetching data:", error);
+        }
         setData(geojsonData as FeatureCollection<Geometry>);
       }
     };
