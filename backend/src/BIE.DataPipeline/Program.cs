2a15cff02bf46bbdc4213cd993530718b7a0fba8
Muhammad Ahsan Khan <ahsan.m.khan@fau.de>
2024-05-28 17:17:56 +0500

    Revert "Finalized the shape parser"
    This reverts commit b0af1b85d12286883b520c46b3b05668ffb571fe.

--- backend/src/BIE.DataPipeline/Program.cs
+++ backend/src/BIE.DataPipeline/Program.cs
@@ -7,48 +7,40 @@ using BIE.DataPipeline.Import;
 Console.WriteLine("Parser Started");
 DataSourceDescription description = YamlImporter.GetSourceDescription(args[0]);
 
-CsvImporter csvImporter = new CsvImporter(description);
+//CsvImporter csvImporter = new CsvImporter(description);
+ShapeImporter shapeImporter = new ShapeImporter(description);
 
 var dbHelper = new DBHelper();
-dbHelper.SetInfo(csvImporter.GetTableName(), csvImporter.GetHeaderString());
-dbHelper.CreateTable(description);
+//dbHelper.SetInfo(csvImporter.GetTableName(), csvImporter.GetHeaderString());
+//dbHelper.CreateTable(description);
 
+dbHelper.SetInfo("SpatialData", "Location ");
+dbHelper.CreateTable(description);
 
 
 //Console.WriteLine(csvImporter.GetHeaderString());
 string line = "";
-bool notEOF = csvImporter.ReadLine(out line);
+//bool notEOF = csvImporter.ReadLine(out line);
+bool notEOF = shapeImporter.ReadLine(out line);
 
 Console.WriteLine("Ready to write.");
 
 var count = 0;
+//while  (notEOF)
+//{
+//    dbHelper.InsertData(line);
+//    notEOF = csvImporter.ReadLine(out line);
+//    count++;
+//    Console.Write($"\rLines: {count}");
+//}
+
 while (notEOF)
 {
     dbHelper.InsertData(line);
-    notEOF = csvImporter.ReadLine(out line);
+    notEOF = shapeImporter.ReadLine(out line);
     count++;
     Console.Write($"\rLines: {count}");
-
 }
 
-
-// Shape is in progress
-if (false)
-{
-    ShapeImporter shapeImporter = new ShapeImporter(description);
-    notEOF = shapeImporter.ReadLine(out line);
-
-    dbHelper.SetInfo("SpatialData", "Location");
-    dbHelper.CreateTable(description, true);
-    count = 0;
-
-    while (notEOF)
-    {
-        dbHelper.InsertData(line);
-        notEOF = shapeImporter.ReadLine(out line);
-        count++;
-        Console.Write($"\rLines: {count}");
-    }
-}
 Console.WriteLine();
 Console.WriteLine("Parser End");
