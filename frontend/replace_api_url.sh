60d9a4dd8c0448228fc7f3f2318d84fa7299737e
Emil Balitzki <emil.balitzki@gmail.com>
2024-06-11 16:27:38 +0200

    Further work on FE deployment
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/replace_api_url.sh
+++ frontend/replace_api_url.sh
@@ -1,4 +1,16 @@
-#!/usr/bin/env sh
-find '/usr/local/apache2/htdocs' -name '*.js' -exec sed -i -e 's,API_GATEWAY_HOST,'"$API_GATEWAY_HOST"',g' {} \;
-find '/usr/local/apache2/htdocs' -name '*.js' -exec sed -i -e 's,API_GATEWAY_PORT,'"$API_GATEWAY_PORT"',g' {} \;
+#!/bin/bash
+
+# Variables for the new values
+NEW_API_GATEWAY_HOST="new_host_value"
+NEW_API_GATEWAY_PORT="new_port_value"
+
+# Directory containing the .js files
+DIRECTORY="."
+
+# Find and replace the API_GATEWAY_HOST and API_GATEWAY_PORT in all .js files
+find "$DIRECTORY" -type f -name "*.js" -exec sed -i \
+    -e "s/API_GATEWAY_HOST/$NEW_API_GATEWAY_HOST/g" \
+    -e "s/API_GATEWAY_PORT/$NEW_API_GATEWAY_PORT/g" {} \;
+
+echo "API Gateway URL replacement completed, starting Apache HTTPD server..."
 httpd-foreground
