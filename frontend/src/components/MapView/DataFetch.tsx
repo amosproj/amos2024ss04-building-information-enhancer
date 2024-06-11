0aeb37b1865247bc6880a43dd0b803605eb3c9ce
Emil Balitzki <emil.balitzki@gmail.com>
2024-06-11 18:35:29 +0200

    Fixed local FE dev server not working
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/src/components/MapView/DataFetch.tsx
+++ frontend/src/components/MapView/DataFetch.tsx
@@ -13,8 +13,8 @@ const geojsonGemeindenPolygons: FeatureCollection =
 
 // These values will be replaced after build with the .sh script when spinning up docker container.
 export const currentEnvironment = {
-  apiBaseHost: "API_GATEWAY_HOST",
-  apiBasePort: "API_GATEWAY_PORT",
+  apiGatewayHost: "API_GATEWAY_HOST",
+  apiGatewayPort: "API_GATEWAY_PORT",
 };
 
 const useGeoData = (
@@ -41,9 +41,15 @@ const useGeoData = (
       default:
         return (
           "http://" +
-          currentEnvironment.apiBaseHost +
+          (currentEnvironment.apiGatewayHost ===
+          ["API_", "GATEWAY_", "HOST"].join()
+            ? currentEnvironment.apiGatewayHost
+            : "localhost") +
           ":" +
-          currentEnvironment.apiBasePort +
+          (currentEnvironment.apiGatewayPort ===
+          ["API_", "GATEWAY_", "PORT"].join()
+            ? currentEnvironment.apiGatewayPort
+            : "8081") +
           "/api/getDatasetViewportData"
         );
     }
