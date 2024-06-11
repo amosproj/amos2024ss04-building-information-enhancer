86f60d066a47bef201b5256a132af612d7623d53
Celine Pöhl <celine.poehl@fau.de>
2024-06-11 16:17:36 +0200

    Content hide/expand
    Signed-off-by: Celine Pöhl <celine.poehl@fau.de>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,29 @@ import "./App.css";
 
 import DataView from "./components/DataView/DataView";
 import MultiMap from "./components/MultiMap/MultiMap";
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import { TabsContext } from "./contexts/TabsContext";
 import MainMenu from "./components/MainMenu/MainMenu";
 import ErrorAlert from "./components/Alerts/ErrorAlert";
 
+import Tooltip from '@mui/material/Tooltip';
+import { CaretLeft, CaretRight } from "@phosphor-icons/react";
+
+import { MapContext } from "./contexts/MapContext";
+
 function App() {
+
+  const { currentMapCache } = useContext(MapContext);
+
   const { currentTabsCache } = useContext(TabsContext);
+
+  const [dataViewVisible, setDataViewVisible] = useState(true);
+
+  const toggleDataView = () => {
+    setDataViewVisible((prevVisible) => !prevVisible);
+    const { mapInstance } = currentMapCache;
+    setTimeout(function(){ mapInstance?.invalidateSize()}, 400);
+  };
 
   return (
     <div className="app-container">
@@ -21,8 +37,25 @@ function App() {
       ) : (
         <Fragment>
           <div className="content-container">
+          <div className={`multi-map ${!dataViewVisible ? "full-width" : ""}`}>
+          <div className="multimap-container">
+          
+          <Tooltip title={dataViewVisible ? "Hide Data View" : "Show Data View"}>
+                  <button onClick={toggleDataView} className="toggle-button">
+                    {dataViewVisible ? (
+                      <CaretRight/>
+                    ) : (
+                      <CaretLeft />
+                    )}
+                  </button>
+                </Tooltip>
+      
             <MultiMap />
+            </div>
+            </div>
+            <div className={`data-view ${!dataViewVisible ? "hidden" : ""}`}>
             <DataView />
+            </div>
           </div>
         </Fragment>
       )}
