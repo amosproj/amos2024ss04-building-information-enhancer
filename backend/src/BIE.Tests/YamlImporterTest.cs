bb3b32f3ed790787c9b1b0f4a610060f321bd816
Nicolas Bandel <nicolas.bandel@fau.de>
2024-06-14 13:46:13 +0200

    Test class for YamlImporter
    Signed-off-by: Nicolas Bandel <nicolas.bandel@fau.de>

---
+++ backend/src/BIE.Tests/YamlImporterTest.cs
@@ -0,0 +1,44 @@
+﻿using BIE.DataPipeline.Import;
+using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BIE.DataPipeline.Import.DataSourceDescription;
+
+namespace BIE.Tests
+{
+    internal class YamlImporterTest
+    {
+        [Test]
+        public void TestGetSourceDescription()
+        {
+            DataSourceDescription expected = new DataSourceDescription();
+            DataSourceLocation expectedLocation = new DataSourceLocation();
+            expectedLocation.type = "filepath";
+            expectedLocation.data_format = "CSV";
+            expectedLocation.location = ".\\..\\..\\..\\testData\\unitTest.csv";
+            expected.source = expectedLocation;
+            expected.delimiter = ';';
+            DataSourceOptions expectedOptions = new DataSourceOptions();
+            expectedOptions.discard_null_rows = false;
+            expectedOptions.skip_lines = 7;
+            expectedOptions.if_table_exists = InsertBehaviour.replace;
+            expected.options = expectedOptions;
+            DataSourceColumn expectedColumn = new DataSourceColumn();
+            expectedColumn.is_not_nullable = false;
+            expectedColumn.name = "colName";
+            expectedColumn.name_in_table = "colTableName";
+            expectedColumn.type = "VARCHAR";
+            expected.table_cols = new List<DataSourceColumn>
+            {
+                expectedColumn,
+            };
+            expected.table_name = "unitTestTable";
+
+            DataSourceDescription actual = YamlImporter.GetSourceDescription(@".\yaml\unitTestShort.yaml");
+            Assert.True(actual.Equals(expected));
+        }
+    }
+}
