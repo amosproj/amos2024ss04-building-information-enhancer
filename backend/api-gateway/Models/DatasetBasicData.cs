e79ff13477606e2c39285b471cc66bebdded4fc7
Emil Balitzki <emil.balitzki@gmail.com>
2024-06-24 20:56:57 +0200

    Fixed frontend metadata object definition
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- backend/api-gateway/Models/DatasetBasicData.cs
+++ backend/api-gateway/Models/DatasetBasicData.cs
@@ -6,5 +6,12 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Icon { get; set; }
+        public List<TableData> Tables { get; set; }
+    }
+
+        public class TableData
+    {
+        public string Name { get; set; }
+        public int NumberOfLines { get; set; }
     }
 }
