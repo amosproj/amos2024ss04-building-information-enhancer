f5e9de7f078fd6c2292bcd4f36ab09c9cb0d5ca3
Nicolas Bandel <nicolas.bandel@fau.de>
2024-06-25 10:06:11 +0200

    WIP started CityGmlImporter (not working yet)
    Signed-off-by: Nicolas Bandel <nicolas.bandel@fau.de>

--- backend/src/BIE.DataPipeline/Import/CityGmlImporter.cs
+++ backend/src/BIE.DataPipeline/Import/CityGmlImporter.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace BIE.DataPipeline.Import
 {
-    internal class CityGmlImporter
+    internal class CityGmlImporter : IImporter
     {
+        private string filePath;
+
+        public CityGmlImporter(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<Building> ReadBuildings()
+        {
+            List<Building> buildings = new List<Building>();
+            XmlDocument doc = new XmlDocument();
+            XmlDocument doc2 = new XmlDocument();
+            doc2.Load(filePath);
+            doc.LoadXml(doc2.InnerXml);
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+            nsmgr.AddNamespace("bldg", "http://www.opengis.net/citygml/building/2.0");
+
+            XmlNodeList buildingNodes = doc.SelectNodes("//bldg:Building", nsmgr);
+            foreach (XmlNode node in buildingNodes)
+            {
+                Console.WriteLine("Building ID: " + node.Attributes["gml:id"].Value);
+                Console.WriteLine("Creation Date: " + node["creationDate"].InnerText);
+                XmlNode externalReference = node["externalReference"];
+                if (externalReference != null)
+                {
+                    Console.WriteLine("Information System: " + externalReference["informationSystem"].InnerText);
+                    XmlNode externalObject = externalReference["externalObject"];
+                    if (externalObject != null)
+                    {
+                        Console.WriteLine("External Object Name: " + externalObject["name"].InnerText);
+                    }
+                }
+            }
+
+            return buildings;
+        }
+        public bool ReadLine(out string nextLine)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+    public class Building
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Positions { get; set; } = new List<string>();
+    }
+    
+
+
+
+
 }
