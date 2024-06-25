fd385da742a081337ae3163ffcf2bcae36af67da
Nicolas Bandel <nicolas.bandel@fau.de>
2024-06-25 09:09:52 +0200

    file setup

---
+++ backend/src/BIE.DataPipeline/Import/CityGmlImporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIE.DataPipeline.Import
+{
+    internal class CityGmlImporter
+    {
+    }
+}
