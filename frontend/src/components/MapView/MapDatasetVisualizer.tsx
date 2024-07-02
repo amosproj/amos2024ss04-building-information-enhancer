4de85f756d630e25f6139dceb99edc9378dd64eb
Emil Balitzki <emil.balitzki@gmail.com>
2024-07-02 16:47:44 +0200

    Added polygons to markers threshold
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/src/components/MapView/MapDatasetVisualizer.tsx
+++ frontend/src/components/MapView/MapDatasetVisualizer.tsx
@@ -1,6 +1,12 @@
 import { useMap } from "react-leaflet";
 import { useCallback, useContext, useEffect } from "react";
-import { FeatureCollection } from "geojson";
+import {
+  Feature,
+  FeatureCollection,
+  GeoJsonProperties,
+  Point,
+  Position,
+} from "geojson";
 import { MapContext } from "../../contexts/MapContext";
 import { TabsContext } from "../../contexts/TabsContext";
 import GeoDataFetcher from "./GeoDataFetcher";
@@ -8,12 +14,12 @@ import L, { DivIcon } from "leaflet";
 import { LatLngBounds } from "leaflet";
 import "proj4leaflet";
 import "proj4";
-import { mergeIcons } from "../../utils/mergeIcons";
 import { createRoot } from "react-dom/client";
 import { flushSync } from "react-dom";
 import { MapPin } from "@phosphor-icons/react";
 import { Dataset } from "../../types/DatasetTypes";
 import { MarkersTypes } from "../../types/MarkersTypes";
+import { createDivIcon } from "../../utils/mergeIcons";
 
 interface MapDatasetVisualizerProps {
   dataset: Dataset;
@@ -29,26 +35,38 @@ const renderToHtml = (Component: React.FC) => {
   return div.innerHTML;
 };
 
-const pinSvg =
-  '<svg width="32" height="32" fill="#000000" viewBox="0 0 256 256" version="1.1" id="svg1" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">  <defs id="defs1" />  <path d="m 128,16 a 88.1,88.1 0 0 0 -88,88 c 0,75.3 80,132.17 83.41,134.55 a 8,8 0 0 0 9.18,0 C 136,236.17 216,179.3 216,104 A 88.1,88.1 0 0 0 128,16 Z m 0,56 a 32,32 0 1 1 -32,32 32,32 0 0 1 32,-32 z" id="path1" />  <ellipse style="fill:#ffffff;stroke:#000000;stroke-width:0;stroke-dasharray:none;stroke-opacity:1" id="path12" cx="128.39645" cy="104.18782" rx="81.751793" ry="81.967773" /></svg>';
-
-const createDivIcon = (iconSvgString: string) => {
-  const combinedSvg = mergeIcons(pinSvg, iconSvgString, 40, 16, 12, 6);
-
-  return L.divIcon({
-    html: combinedSvg,
-    className: "", // Optional: add a custom class name
-    iconSize: [40, 40],
-    iconAnchor: [20, 20], // Adjust the anchor point as needed
-  });
-};
-
 const divIcondefault: DivIcon = L.divIcon({
   html: renderToHtml(() => <MapPin size={32} weight="duotone" />),
   className: "", // Optional: add a custom class name
   iconSize: [34, 34],
   iconAnchor: [17, 17], // Adjust the anchor point as needed
 });
+
+// Function to convert polygon GeoJSON to marker GeoJSON
+const convertPolygonsToMarkers = (
+  geoData: FeatureCollection
+): FeatureCollection<Point, GeoJsonProperties> => {
+  const markerFeatures: Feature<Point, GeoJsonProperties>[] =
+    geoData.features.map((feature) => {
+      if (feature.geometry.type === "Polygon") {
+        const firstCoord = feature.geometry.coordinates[0][0];
+        return {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: firstCoord as Position,
+          },
+          properties: feature.properties,
+        } as Feature<Point, GeoJsonProperties>;
+      }
+      return feature as Feature<Point, GeoJsonProperties>;
+    });
+
+  return {
+    type: "FeatureCollection",
+    features: markerFeatures,
+  };
+};
 
 const MapDatasetVisualizer: React.FC<MapDatasetVisualizerProps> = ({
   dataset,
@@ -114,8 +132,10 @@ const MapDatasetVisualizer: React.FC<MapDatasetVisualizerProps> = ({
         }
       } else {
         console.log("mark");
+        // Convert polygons to markers
+        const markerData = convertPolygonsToMarkers(geoData);
         // Add the markers to the map instead
-        const geojsonLayer = L.geoJson(geoData, {
+        const geojsonLayer = L.geoJson(markerData, {
           pointToLayer: function (_feature, latlng) {
             return L.marker(latlng, {
               icon: dataset.metaData
--- frontend/src/utils/mergeIcons.ts
+++ frontend/src/utils/mergeIcons.ts
@@ -1,3 +1,5 @@
+import L from "leaflet";
+
 export const modifySvg = (svgString: string, size: number = 30) => {
   const svgElement = new DOMParser()
     .parseFromString(svgString, "image/svg+xml")
@@ -39,4 +41,18 @@ export const mergeIcons = (
     </div>
   `;
   return combinedHtml;
+};
+
+export const pinSvg =
+  '<svg width="32" height="32" fill="#000000" viewBox="0 0 256 256" version="1.1" id="svg1" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">  <defs id="defs1" />  <path d="m 128,16 a 88.1,88.1 0 0 0 -88,88 c 0,75.3 80,132.17 83.41,134.55 a 8,8 0 0 0 9.18,0 C 136,236.17 216,179.3 216,104 A 88.1,88.1 0 0 0 128,16 Z m 0,56 a 32,32 0 1 1 -32,32 32,32 0 0 1 32,-32 z" id="path1" />  <ellipse style="fill:#ffffff;stroke:#000000;stroke-width:0;stroke-dasharray:none;stroke-opacity:1" id="path12" cx="128.39645" cy="104.18782" rx="81.751793" ry="81.967773" /></svg>';
+
+export const createDivIcon = (iconSvgString: string) => {
+  const combinedSvg = mergeIcons(pinSvg, iconSvgString, 40, 16, 12, 6);
+
+  return L.divIcon({
+    html: combinedSvg,
+    className: "", // Optional: add a custom class name
+    iconSize: [40, 40],
+    iconAnchor: [20, 20], // Adjust the anchor point as needed
+  });
 };
