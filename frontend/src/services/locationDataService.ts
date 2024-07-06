3f678e12859d533d6702376733f933de774fb025
Emil Balitzki <emil.balitzki@gmail.com>
2024-07-06 16:14:12 +0200

    Implemented the open as a map functionality
    Signed-off-by: Emil Balitzki <emil.balitzki@gmail.com>

--- frontend/src/services/locationDataService.ts
+++ frontend/src/services/locationDataService.ts
@@ -19,10 +19,65 @@ export const fetchLocationData = async (
     location: location,
   };
   try {
-    const response = await axios.put<LocationDataResponse>(
-      getAPIGatewayURL() + "/api/loadLocationData",
-      requestBody
-    );
+    // const response = await axios.put<LocationDataResponse>(
+    //   getAPIGatewayURL() + "/api/loadLocationData",
+    //   requestBody
+    // );
+
+    const response: LocationDataResponse = {
+      currentDatasetData: [
+        {
+          key: "temperature",
+          value: "22°C",
+          mapId: "",
+        },
+        {
+          key: "humidity",
+          value: "45%",
+          mapId: "",
+        },
+        {
+          key: "windSpeed",
+          value: "15 km/h",
+          mapId: "",
+        },
+      ],
+      generalData: [
+        {
+          key: "population",
+          value: "1,000,000",
+          mapId: "map004",
+        },
+        {
+          key: "area",
+          value: "500 sq km",
+          mapId: "",
+        },
+        {
+          key: "elevation",
+          value: "300 meters",
+          mapId: "",
+        },
+      ],
+      extraRows: [
+        {
+          key: "timeZone",
+          value: "GMT+2",
+          mapId: "",
+        },
+        {
+          key: "language",
+          value: "English",
+          mapId: "EV_charging_stations",
+        },
+        {
+          key: "currency",
+          value: "USD",
+          mapId: "",
+        },
+      ],
+    };
+    return response;
 
     if (response.status === 200) {
       return response.data;
