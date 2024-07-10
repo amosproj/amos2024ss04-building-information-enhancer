05acce7761e6dbdf6fb6c48487126e1c245fd30c
Muhammad Ahsan Khan <ahsan.m.khan@fau.de>
2024-07-10 20:00:11 +0200

    Created basic script to generate yaml files for the air polutants dataset
    Signed-off-by: Muhammad Ahsan Khan <ahsan.m.khan@fau.de>

---
+++ backend/src/BIE.DataPipeline/air_pollutants.py
@@ -0,0 +1,69 @@
+﻿import os
+
+def create_yaml_files(input_file, output_dir):
+    with open(input_file, 'r', encoding="utf-8") as infile:
+        for i, line in enumerate(infile):
+            parts = line.strip().split(",")
+
+            link = parts[0]
+            name = parts[1].replace(" ", "_")
+
+            name = name.replace(u'.', "_")
+            name = name.replace(u'-', "_")
+            name = name.replace(u'ä', "ae")
+            name = name.replace(u'Ä', "Ae")
+            name = name.replace(u'ö', "oe")
+            name = name.replace(u'Ö', "Oe")
+            name = name.replace(u'ü', "ue")
+            name = name.replace(u'Ü', "Ue")
+            name = name.replace(u'ß', "ss")
+            name = name.replace(u'(', "")
+            name = name.replace(u')', "")
+            
+            # Generate yaml file contents
+            file_content = f"""# describe the source
+            source:
+              # link | filepath
+              type: URL
+              location: {link}
+              data_format: CSV
+            options:
+              # skip lines at the beginning
+              skip_lines: 0
+              # discard any rows that have null values
+              discard_null_rows: false
+              # how to deal with existing table. Options: ignore, replace, skip (default).
+              if_table_exists: skip
+              location_to_SQL_point: null
+            table_name: actual_use_{name}
+
+            table_cols:
+              - name: Bundesland / Messnetz
+                name_in_table: state_network
+              - name: Stationscode
+                name_in_table: station_code
+              - name: Stationsname
+                name_in_table: station_name
+              - name: Stationsumgebung
+                name_in_table: station_environment
+              - name: "Art der Station"
+                name_in_table: station_type
+              - name: "Jahresmittelwert in µg/m³"
+                name_in_table: annual_mean_ugm3
+                type: DECIMAL(5, 2)
+              - name: "Zahl der Stundenmittelwerte über 200 µg/m³"
+                name_in_table: hourly_means_over_200ugm3
+                type: INT
+            """
+            
+            output_file = os.path.join(output_dir, f'air_pollutants_{name}.yaml')
+            
+            with open(output_file, 'w') as outfile:
+                outfile.write(file_content)
+                
+            print(f'Created: {output_file}')
+
+input_file = 'air_pollutants_datasets'
+output_dir = 'yaml'
+
+create_yaml_files(input_file, output_dir)
